4d7781f696974da46834c31f49bfcd3cb8670f74
Peter Maltby <peter.maltby1@sainsburys.co.uk>
2023-06-19 11:38:39 +0100

    added pnpm stuff

--- hosts/HOLBMAC2259.zsh
+++ hosts/HOLBMAC2259.zsh
@@ -25,3 +25,13 @@ neofetch
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# pnpm
+export PNPM_HOME="/Users/peter.maltby1/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
+
+alias npm='echo "use pnpm for now and see how it goes"'
