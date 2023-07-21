e7781a9b2eef7fee8ba712310ea460d5d4a49068
Peter Maltby <contact@petermaltby.com>
2023-07-21 12:39:26 +0100

    added some tmeplates

---
+++ .config/nvim/templates/skeleton.svelte
@@ -0,0 +1,16 @@
+<!--
+[:EVAL:]expand("%:t")[:END:]
+author: [:EVAL:]$USER[:END:]
+created: [:EVAL:]strftime('%Y-%m-%d')[:END:]
+[:EVAL:]system("figlet -f small -k " . toupper(expand('%:t:r')))[:END:]-->
+<script lang="typescript" >
+
+</script>
+
+<div>
+    <p>new component</p>
+</div>
+
+<style land="scss">
+
+</style>
