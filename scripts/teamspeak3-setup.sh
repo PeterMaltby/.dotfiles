eaa5dfd2a9b4d7b7e308a5f917bfedf45f90ed80
Peter Maltby <contact@petermaltby.com>
2023-09-30 09:37:57 +0100

    finished v1 of teamspeak-setup

--- scripts/teamspeak3-setup.sh
+++ scripts/teamspeak3-setup.sh
@@ -11,13 +11,14 @@ downloadedFiles="${tmpDir}/teamspeakExtract"
 
 teamspeakHomeDir="/opt/teamspeak"
 
+tmpServiceFile="${tmpDir}/teamspeak.service"
+systemServiceFile="/lib/systemd/system/teamspeak.service"
 #############################################################
 pStart
 
 pLog "installing pre reqs"
 sudo dnf install -y perl tar net-tools bzip2
 pCheckError $? "dnf"
-
 
 pLog "creating teamspeak user"
 sudo useradd teamspeak -d $teamspeakHomeDir
@@ -28,15 +29,45 @@ curl ${downloadUrl} --output "${downloadedTar}"
 pCheckError $? "curl"
 
 pLog "extracting tar"
+touch "${downloadedFiles}"
 tar -xvf "${downloadedTar}" -C "${downloadedFiles}"
 pCheckError $? "tar"
 
 pLog "moving files to ${teamspeakHomeDir}"
-sudo mv "${downloadedFiles}/*" ${teamspeakHomeDir}
+sudo mv ${downloadedFiles}* ${teamspeakHomeDir}
 pCheckError $? "mv"
+
+pLog "agreeing to licence"
+sudo touch "${teamspeakHomeDir}/.ts3server_license_accepted"
 
 pLog "changing owner of ${teamspeakHomeDir} to teamspeak user"
 sudo chown -R teamspeak: ${teamspeakHomeDir}
 pCheckError $? "chown"
 
+pLog "creating service file"
+cat > "${tmpServiceFile}" << EOF
+[Unit]
+Description=Team Speak 3 Server
+After=network.target
+[Service]
+WorkingDirectory=${teamspeakHomeDir}
+User=teamspeak
+Group=teamspeak
+Type=forking
+ExecStart=${teamspeakHomeDir}/ts3server_startscript.sh start inifile=ts3server.ini
+ExecStop=${teamspeakHomeDir}/ts3server_startscript.sh stop
+PIDFile=${teamspeakHomeDir}/ts3server.pid
+RestartSec=15
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOF
+pCheckError $? "cat"
 
+pLog "moving service file to ${systemServiceFile}"
+sudo mv "${tmpServiceFile}" ${systemServiceFile}
+pCheckError $? "mv"
+
+pLog "service created successfully! please enable and start the systemctl service"
+
+pEnd
