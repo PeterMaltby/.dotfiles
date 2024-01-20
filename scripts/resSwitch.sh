e4020e8700c75530c315e60fd22a7c94d3697daa
Peter Maltby <contact@petermaltby.com>
2024-01-20 09:26:48 +0000

    alacritty config update, resSwitch fix

--- scripts/resSwitch.sh
+++ scripts/resSwitch.sh
@@ -12,22 +12,22 @@ monitorName="HDMI-A-0"
 pStart
 
 xrandrOutput=$(xrandr | grep $monitorName)
-resolution=$(echo "$xrandrOutput" | awk '{print $4}')
+resolution=$(echo "$xrandrOutput" | awk '{print $3}')
 
-if [ "${resolution}" == "3840x2160+1080+0" ]; then
+if [ "${resolution}" == "3840x2160+0+0" ]; then
     pLog "setting display to 1080p"
     echo "setting display to 1080p"
     xrandr --output HDMI-A-0 --mode 1920x1080
 fi
 
-if [ "${resolution}" == "1920x1080+1080+0" ]; then
+if [ "${resolution}" == "1920x1080+0+0" ]; then
     echo "setting display to 4k"
     pLog "setting display to 4k"
     xrandr --output HDMI-A-0 --mode 3840x2160
 fi
 
-if [ "${resolution}" != "3840x2160+1080+0" ] && \
-   [ "${resolution}" != "1920x1080+1080+0" ]; then
+if [ "${resolution}" != "3840x2160+0+0" ] && \
+   [ "${resolution}" != "1920x1080+0+0" ]; then
     pError "resolution was: \"${resolution}\"!" 
 fi
 
