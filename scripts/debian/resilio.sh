faa63a63fbd06783ee0f23b5fcb78454f1057d50
Peter Maltby <contact@petermaltby.com>
2024-03-20 16:32:57 +0000

    config generator added

--- scripts/debian/resilio.sh
+++ scripts/debian/resilio.sh
@@ -9,12 +9,10 @@ sourceListEntry="deb http://linux-packages.resilio.com/resilio-sync/deb resilio-
 sourcePubKeyUrl="https://linux-packages.resilio.com/resilio-sync/key.asc"
 tmpKeyFile="${tmpDir}/key.asc"
 
-configFile="${inputDir}/sync.conf"
-configTemplateFile="$S/.share/resilio.template"
+configFile="${tmpDir}/config.json"
 
-serviceFile="${outputDir}/resilio-sync.service"
 
-syncStorage="${HOME}/.config/rslsync"
+syncStorage="${HOME}/rslsync"
 #############################################################
 pStart
 
@@ -22,29 +20,72 @@ pLog "adding resilio source to sources list"
 echo $sourceListEntry | sudo tee -a /etc/apt/sources.list
 pCheckError $? "adding source to source entry list"
 
-pLog "getting and adding source key"
-curl $sourcePubKeyUrl > "$tmpKeyFile"
+pLog "getting and adding gpg key from $sourcePubKeyUrl"
+wget -qO- $sourcePubKeyUrl | sudo tee /etc/apt/trusted.gpg.d/resilio.asc
 pCheckError $? "curl pub key"
 
-pLog "adding key to apt"
-sudo apt-key add $tmpKeyFile
+pLog "installing resilio"
+sudo apt update
+sudo apt install resilio-sync
 
-pEnd
-
-if [ ! -f "${configFile}" ]; then
-    pLog "config file ($configFile) not created creating template"
-    cp "$configTemplateFile" "$configFile"
-    sed -i "s/DEVICE_NAME/${USER}@${hostName}/g" "$configFile"
-    sed -i "s/STORAGE_PATH/${syncStorage}/g" "$configFile"
-    pLog "template config file created at $configFile please populate then rerun"
-    pEnd
+if [ ! -f "${keysFile}" ]; then
+	pLog "populate ${keysFile} with keys for folders you wish to sync, then rerun"
+	pEnd
 fi
 
-pLog "config file located at $configFile is being installed"
+pLog "constructing config File"
+touch "${configFile}"
 
-mkdir -p "$syncStorage"
-cp "$configFile" "$syncStorage/rslsync.conf"
-pCheckError $? "cp config file to $syncStorage"
+cat > "${configFile}" << EOF
+{
+	"device_name": "${hostName}",
+	"listening_port": 8888,
+        "pid_file": "/run/resilio-sync/sync.pid",
+	"storage_path": "${syncStorage}/.sync",
+	"use_upnp": false,
+
+	"shared_folders" :
+	[
+EOF
+
+# to deal with json last line of list stuff
+first=true
+
+while read -r key; do
+	if [[ ! "${key}" =~ ${keyCheckRegex} ]]; then
+		pError "invalid key: \"${key}\" in file ${keysFile}"
+	fi
+
+	if [ ${first} == true ]; then
+		first=false
+	else
+		echo "	}," >> "${configFile}"
+	fi
+
+	cat >> "${configFile}" << EOF
+	{
+		"secret": "${key}",
+		"dir": "${syncStorage}/${key}",
+		"use_relay_server" : true,
+		"use_tracker": false,
+		"search_lan": false,
+		"use_sync_trash" : false,
+		"overwrite_changes": false,
+		"selective_sync": false
+EOF
+
+done < "${keysFile}"
+
+cat >> "${configFile}" << EOF
+	}
+	]
+}
+EOF
+
+pLog "Config file created succesfully at: ${configFile}"
+cat ${configFile}
+
+mv ${configFile} 
 
 pLog "service created sucessfully! please enable and start the systemctl service"
 
